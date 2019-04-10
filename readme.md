d51840c7ebdc8be0e70f39e45f1313db3e615702
Yolopix <13918281+y0lopix@users.noreply.github.com>
2019-04-10 19:39:42 +0200

    Rewrite in Python

--- readme.md
+++ readme.md
@@ -1,12 +1,9 @@
 # Thunkable to Kodular AIA converter
 
-Only working on Windows
+**WARNING : The Python version currently creates an invalid AIA file for Kodular because of the new line format (CRLF instead of LF)**
 
 How to use:
  1. Export your project on the latest version of Thunkable
- 2. Put the exported AIA file in the same folder as the converter and drag it to thunkable2kodular.cmd
- 3. A new file called *_kodular.aia has been created
+ 2. Drag the exported AIA file to thunkable2kodular.py
+ 3. A new file called *_kodular.aia will be created
  4. Go to Kodular and import the new AIA file
- 5. That's all. Don't hesitate to report an issue if something doesn't work.
-
-I plan to rewrite the converter in another language (most likely in Python) because of the limitations of the Windows scripting language that truncates big AIA files.
---
+++ thunkable2kodular.py
@@ -0,0 +1,46 @@
+#encoding: utf-8
+from sys import argv
+from os import path,mkdir,walk,chdir
+from shutil import rmtree
+from zipfile import is_zipfile,ZipFile,ZIP_DEFLATED
+from glob import iglob
+from fileinput import FileInput
+from ntpath import basename
+
+replace_list=[["ThunkablePushNotification","PushNotifications"],["ThunkableFloatingActionButton","MakeroidFab"],["ThunkableAdMobInterstitial","AdMobInterstitial"],["ThunkableAdMobBanner","AdmobBanner"],["ThunkableSwitch\",\"$Version\":\"6=","SwitchToggle\",\"$Version\":\"3"],["ThunkableSwitch","SwitchToggle"]]
+
+if len(argv)==2:
+    print("Converting "+argv[1]+" to Kodular...")
+    temp_folder_name="."+basename(argv[1])+"_temp"
+    if path.isfile(argv[1]):
+        if path.exists(temp_folder_name):
+            rmtree(temp_folder_name)
+        mkdir(temp_folder_name)
+        if is_zipfile(argv[1]):
+            aia_file=ZipFile(argv[1])
+            aia_file.extractall(path=temp_folder_name)
+            aia_file.close()
+            #=== I need to change this part because LF are converted to CRLF
+            for i in iglob(temp_folder_name+"/src/com/*/*/*",recursive=1):
+                for t in replace_list:
+                    for line in FileInput(i,inplace=1):
+                        print(line.replace(t[0],t[1]),end="")
+            #===
+            extension=path.splitext(argv[1])[1]
+            new_file_name=path.splitext(argv[1])[0]
+            new_aia_file=ZipFile(new_file_name+"_kodular"+extension,"w",ZIP_DEFLATED)
+            temp_dir_path=path.realpath(temp_folder_name)
+            chdir(temp_folder_name)
+            print(temp_dir_path)
+            for dirs,sdirs,files in walk(temp_dir_path):
+                for f in files:
+                    new_aia_file.write(path.relpath(dirs)+"/"+f)
+            new_aia_file.close()
+            chdir("..")
+            rmtree(temp_folder_name)
+            print("Done! You can import "+new_file_name+"_kodular"+extension+" in Kodular.")
+        else:
+            print(argv[1]+" is not a valid AIA file")
+            rmtree(temp_folder_name)
+else:
+    print("Drop an AIA file to "+path.basename(argv[0])+" or type \""+path.splitext(path.basename(argv[0]))[0]+" <AIA file>\" in the terminal")
